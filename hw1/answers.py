e6bac5e8126cca137d9006af6d0afa64d53e8496
Assaf-Haik-Barouch <Assaf98@gmail.com>
2019-04-10 09:21:07 +0200

    answered 1 question + added template for the rest in part 3

--- hw1/answers.py
+++ hw1/answers.py
@@ -23,6 +23,10 @@ That happens because we are looking at too many points in the training set, even
 
 part3_q1 = r"""
 **Your answer:**
+The choice of $\Delta > 0$ is arbitrary for the SVM loss $L(W)$ because
+<INSERT ANSWER>
+
+
 
 
 Write your answer using **markdown** and $\LaTeX$:
@@ -36,6 +40,12 @@ An equation: $e^{i\pi} -1 = 0$
 
 part3_q2 = r"""
 **Your answer:**
+1) We interpret what the model is actually learning as
+<INSERT ANSWER>
+
+2) This is different from KNN because
+<INSERT ANSWER>
+
 
 
 Write your answer using **markdown** and $\LaTeX$:
@@ -49,15 +59,16 @@ An equation: $e^{i\pi} -1 = 0$
 
 part3_q3 = r"""
 **Your answer:**
-
-
-Write your answer using **markdown** and $\LaTeX$:
-```python
-# A code block
-a = 2
-```
-An equation: $e^{i\pi} -1 = 0$
-
+1) Based on the graph of the training set loss We would say that  
+our learning rate is **good** as the value of the average loss converged to  
+small values under 1 after 5-10 epochs and kept getting better afterwards.  
+If the learning rate was **too low** the graph would show much slower convergence,  
+and if the learning rate was **too high** the loss would probably not converge at all.  
+  
+2) Based on the graph of the Training and validation sets' accuracy we would say that  
+the model is **slightly overfitted to the training set** as we can see that  
+the accuracies of the 2 sets converge into 2 different values, and the test set's value  
+is slightly higher than the validation set's. 
 """
 
 # ==============
